c77bc27728bcfa71f51abfb685ff2048c4afa2ff
chief666 <787579223@qq.com>
2019-04-18 09:23:35 +0800

    vue4.18

---
+++ vue-router-template/src/components/index02.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+	.container{
+		display: flex;
+		background-color: #808080;
+	}
+	.f{
+		width: 500px;
+		height: 200px;
+		
+	}
+</style>
